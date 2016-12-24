bedab7a401ec0ddde4922597d5416b0803cbe68e
Takafumi Yoshida <zephiransas@gmail.com>
2016-12-24 20:23:30 +0900

    Implement live result list

--- src/main/java/io/github/zephiransas/controller/WelcomeController.java
+++ src/main/java/io/github/zephiransas/controller/WelcomeController.java
@@ -1,15 +1,21 @@
 package io.github.zephiransas.controller;
 
+import io.github.zephiransas.repository.LiveResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class WelcomeController {
 
-    @RequestMapping
-    public String index() {
+    @Autowired
+    LiveResultRepository repository;
 
+    @RequestMapping
+    public String index(Model model) {
+        model.addAttribute("results", repository.findAllByOrderByCreatedAtDesc());
         return "index";
     }
 }
--- src/main/java/io/github/zephiransas/repository/LiveResultRepository.java
+++ src/main/java/io/github/zephiransas/repository/LiveResultRepository.java
@@ -4,6 +4,9 @@ import io.github.zephiransas.model.LiveResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LiveResultRepository extends JpaRepository<LiveResult, Integer> {
+    List<LiveResult> findAllByOrderByCreatedAtDesc();
 }
